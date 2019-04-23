82ab7edcd7ca09e07d828c31e2131e07427c44e7
Home <home@MacBook-Pro-de-Alexandre-Pereira.local>
2019-04-23 13:32:26 +0100

    Tempurari page

---
+++ src/components/Logotipo.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="col-12">
+        <img alt="Vue logo" src="../assets/img/main/Logo.png" height="100%" width="100%">
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+img{
+    position: relative;
+    z-index: 3;
+}
+</style>
+
